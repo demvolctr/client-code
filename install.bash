eee09ae41c2b4ddc758bf9920f959e11fac05b30
Jon Hull <hull39@gmail.com>
2016-09-07 10:19:40 -0700

    testing spin on net if

--- install.bash
+++ install.bash
@@ -1,5 +1,36 @@
 #!/bin/bash
+#
+#  install.bash:
+#    1.  spin on network interface first, then github
+#    2.  if both are alive, then pull and update
+#
+
+test_host() {
+   for i in `seq 1 10`;
+   do
+      ping -q -c 1 $1 > /dev/null
+      let exit_status=$?
+      if [ $exit_status -eq 0 ]; then echo "found $1 i=$i"; return 0; fi;
+      sleep 10
+   done
+   return 1
+}
+
+test_host "127.0.0.1"
+let host_there=$?
+if [ $host_there -ne 0 ]; then echo "no net interface"; exit 1; fi
+
+test_host "github.com"
+let host_there=$?
+if [ $host_there -ne 0 ]; then echo "no github.com"; exit 1; fi
+
+echo `/bin/date` "Starting pull and install"
 
 cd /home/dem/gitproject/client-code
 git pull https://github.com/demvolctr/client-code
+let pull_result=$?
+if [ $pull_result -eq 0 ]; then
+   echo "Nothing changed since last reboot"
+   exit 0
+fi
 /home/dem/gitproject/client-code/copy_usr_local_bin.pl
