cae53c518b87bddb7b8e9698b3f9793b1131989c
demvolctr <hull39@icloud.com>
2016-11-01 13:09:45 -0700

    refresh_display.bash

---
+++ usr_local_bin/refresh_display.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+#  
+#  refresh_display.bash: press the left mouse button to refresh the display
+#
+#      Jon Hull -- 11/1/16
+#      usage: when logged in as demcaller: ./refresh_display.bash
+#
+export Xauthority="/var/run/lightdm/root:0"
+export DISPLAY=":0"
+xdotool click 1
